aab3878c12b38fadf2c31d806a08c6bffdc65d8c
Jin Woo Kim <jinwoov@gmail.com>
2020-01-13 15:29:00 -0800

    added time out

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,6 +4,8 @@ let click = 0;
 
 $(document).ready( function() {
     $("#top").css("background-image", "url('./images/main-image1.jpeg')")
+    $(".alt").css("color", "white")
+    $(".alt").css("textShadow", "1px 2px 2px black")
 })
 
 
@@ -12,18 +14,33 @@ $('button').click( function (event) {
     event.preventDefault();
     // console.log('pof')
     let select = $(this).val();
-    if (click == 0) {
-    $("#top").css("background-image", "url('./images/beach.jpeg')")
-    click++;
-     } else if (click === 1) {
-        $("#top").css("background-image", "url('./images/honu.jpeg')")
-        click++;
-     } else if (click === 2) {
-     $("#top").css("background-image", "url('./images/main-image1.jpeg')")
-     click=0;
-     }
+    imageChange()
     })
 }
+
+const imageChange = () =>{
+    if (click == 0) {
+        $("#top").css("background-image", "url('./images/beach.jpeg')")
+        $(".alt").css("color", "white")
+        click++;
+         } else if (click === 1) {
+            $("#top").css("background-image", "url('./images/honu.jpeg')")
+            $(".alt").css("color", "white")
+            click++;
+         } else if (click === 2) {
+         $("#top").css("background-image", "url('./images/main-image1.jpeg')")
+         $(".alt").css("color", "white")
+    
+         click=0;
+         }
+}
+
+window.onload = function() {
+    var x = 0;
+    setInterval(function() {
+        imageChange();
+    },5000);
+  }   
 
 
 
